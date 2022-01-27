0a3f3423c8a13e3dc5c5395b587bdc9bad9fb652
Brian Celine <brianceline@gmail.com>
2022-01-27 00:10:38 +0000

    Javascript Beginner

---
+++ arrays.js
@@ -0,0 +1,31 @@
+# what is an array
+
+==================
+An array is a special variable, which can hold more than one value.
+
+# creating an array
+
+===================
+let names = ['Ryan', 'Eric', 'Stephen', 'Laura'];
+
+// retrieve values from array
+names[1];
+
+names.length;
+
+# changing array values
+
+=======================
+let names = ['Ryan', 'Eric', 'Stephen', 'Laura'];
+names.push('Janet');
+
+# deleting items in array
+
+=========================
+let names = ['Ryan', 'Eric', 'Stephen', 'Laura'];
+delete names[1];
+
+# further reading
+
+=================
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
---
+++ play.js
@@ -0,0 +1,59 @@
+// Variables in Js can be defined using: const, let, var
+// The below will print kingsley
+
+var name = "kingsley";
+
+console.log(name);
+
+
+// The below will still print kingsley
+
+let name = "kingsley";
+
+console.log(name);
+
+
+// The below will still print kingsley
+
+const name = "kingsley";
+
+console.log(name);
+
+
+
+// In javascript there is scope. When using let, you can no longer access the variable.
+
+{
+  let moo = "Moo"
+
+}
+
+console.log(moo);
+
+
+// In javascript there is scope. When using var, you can access the variable.
+
+{
+  var moo = "Moo"
+
+}
+
+console.log(moo);
+
+
+
+// In javascript there is scope. When using const, you cannot access the variable. Also when defining the const, you would use Capital letters.
+
+{
+  const MOO = "Moo"
+
+}
+
+console.log(moo);
+
+// ************************************
+// Using var is more lenient than using const or let
+
+// When you create a const the value cannot be changed afterwards.
+// When you create a let the value can be changed.
+// When you create a var the value can be changed.
