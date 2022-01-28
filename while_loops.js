f05abb1cd87294b6bcc51a76f6a7cb38c1a3cdfd
Brian Celine <brianceline@gmail.com>
2022-01-28 22:52:07 +0000

    While Loops

---
+++ while_loops.js
@@ -0,0 +1,31 @@
+
+# while condition
+=================
+
+let i = 1;
+let n = 5;
+
+while (i <= n) {
+  console.log(i)
+  i++;
+}
+
+# do while condition
+====================
+let i = 1;
+let n = 5;
+
+do {
+  console.log(i)
+  i++;
+} while (i <= n);
+
+# infinite loop
+===============
+while(true) {
+  console.log('hello')
+}
+
+# further reading
+=================
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/break
