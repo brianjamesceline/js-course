1d6b57dbe0c35eadc397f4f50bfb1df28d197310
Brian Celine <brianceline@gmail.com>
2022-01-28 22:08:33 +0000

    Increment/Decrement

---
+++ increment_or_decrement.js
@@ -0,0 +1,19 @@
+// # post fix increment
+// ====================
+let i = 5;
+i++;
+
+let j = 10;
+j--;
+
+// # prefix increment
+// ==================
+let i = 5;
+++i;
+
+let j = 10;
+--j;
+
+// # further reading
+// =================
+// https:developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
